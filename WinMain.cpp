32bb064bf99e263742169d82d820dceed1943a33
Alexander <sashalivanov2007@gmail.com>
2022-11-27 21:27:19 +0300

    waiting for a fix with hInstance

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,3 +1,15 @@
+#include <Windows.h>
+
 int main(){
     return 0;
+
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    RegisterClassA();
 }
