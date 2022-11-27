12d0f7b05d252fcacb43e8984e159e3a6daf8934
Alexander <sashalivanov2007@gmail.com>
2022-11-27 11:23:59 +0300

    Fix with libs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "GLFW/glfw3.h"
 
 using namespace std;
 
 int main(){
-    cout << "Hi Bro!\n";
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "3D Engine", sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(60);
+    getchar();
+    return 0;
 }
