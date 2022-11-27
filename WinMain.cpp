75a037babbe8a157906ab27e3146d637082fc506
Alexander <sashalivanov2007@gmail.com>
2022-11-27 21:59:53 +0300

    now window shows, but it frozzen because I have inf.cycle... haha :)

--- WinMain.cpp
+++ WinMain.cpp
@@ -21,7 +21,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     RegisterClassEx(&wc);
 
     HWND hWnd = CreateWindowEx(0, pClassName, pClassName, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, 200, 200, 640, 480, nullptr, nullptr, hInstance, nullptr);
-
+    ShowWindow(hWnd, SW_SHOW);
     while (true);
     return 0;
 }
